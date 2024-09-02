80b613686c455585d41d16d22f3c26627f8f5ec3
gabriellafguerra21 <gabriellafguerra21@gmail.com>
2024-09-02 15:38:46 +0000

    Created resource controllers

---
+++ app/Http/Controllers/TaskTagController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task_Tag;
+use Illuminate\Http\Request;
+
+class TaskTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task_Tag $task_Tag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task_Tag $task_Tag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task_Tag $task_Tag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task_Tag $task_Tag)
+    {
+        //
+    }
+}
